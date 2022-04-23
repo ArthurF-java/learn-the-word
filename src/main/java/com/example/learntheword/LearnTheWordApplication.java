2df5e60bc4117070d647d94a6500e4845f1ce306
Arthur_Fonichkin <fonichkin.arthur@gmail.com>
2022-04-23 20:32:01 +0300

    1

---
+++ src/main/java/com/example/learntheword/LearnTheWordApplication.java
@@ -0,0 +1,13 @@
+package com.example.learntheword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTheWordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnTheWordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learntheword/LearnTheWordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learntheword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTheWordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
